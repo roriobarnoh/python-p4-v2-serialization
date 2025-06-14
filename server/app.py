1df0d94ab186ce8d0f68c0c4deb315cbb3419fc3
roriobarnoh <roriokiptoo2016@gmail.com>
2025-06-14 19:49:38 +0300

    serialization

--- server/app.py
+++ server/app.py
@@ -21,5 +21,33 @@ def index():
     return make_response(body, 200)
 
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter_by(id=id).first()
+
+    if pet:
+        body = pet.to_dict()
+        response_status = 200
+    else:
+        body = {'error' : 'Pet not found'}
+        response_status = 404
+    return make_response(body, response_status)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []
+    
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+
+    body = {
+        'count': len(pets),
+        'pets': pets
+    }
+    return make_response(body, 200)
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
